ea409e47a060f655ae5bf8b7bcf1a183b5fa306f
joseph skokan <joeskokan20@gmail.com>
2025-01-12 00:23:48 -0500

    add crud

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -29,7 +29,50 @@ const createProduct = async (req, res) => {
   }
 };
 
+// Update an existing product
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, sku, price, quantity, category } = req.body;
+    const updatedProduct = await Product.updateProduct({
+      id,
+      name,
+      sku,
+      price,
+      quantity,
+      category,
+    });
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(updatedProduct);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+// Delete a product
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.deleteProduct(id);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json({
+      message: "Product deleted successfully",
+      product: deletedProduct,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
 module.exports = {
   getProducts,
   createProduct,
+  updateProduct,
+  deleteProduct,
 };
