355d66c92df3c775d8c9bf857e91b7aadb898b65
joseph skokan <joeskokan20@gmail.com>
2025-01-11 23:44:44 -0500

    front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Inventory Management System</h1>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Products.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  // Fetch products from the backend
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the products!", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
