e1a1e0ff740d690d800b309b848bfd05558b1cda
joseph skokan <joeskokan20@gmail.com>
2025-01-17 23:00:26 -0500

    changed home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
 
   return (
     <div className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg mt-10">
-      <h2 className="text-2xl font-semibold text-center mb-4">Inventory WIP</h2>
-      <h4 className="text-lg text-center text-gray-600 mb-6">Placeholder</h4>
+      <h2 className="text-2xl font-semibold text-center mb-4">Company Name</h2>
+      <h4 className="text-lg text-center text-gray-600 mb-6">Inventory</h4>
 
       {/* Button to add product */}
       <div className="flex justify-center">
